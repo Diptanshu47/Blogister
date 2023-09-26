ec35fc13d5d9e3cfd23ed96b584a5a280afdcd6e
“Diptanshu47” <goswamidiptanshu@gmail.com>
2023-09-26 17:18:40 +0530

    some tweaks

--- Blog Frontend/src/components/Header.jsx
+++ Blog Frontend/src/components/Header.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-default">
-    <div className="container">
+      <div className="container">
         <div className="navbar-header">
-        <p className="navbar-brand">BLOGISTER</p>
+          <p className="navbar-brand">BLOGISTER</p>
         </div>
         <ul className="nav navbar-nav navbar-right">
             <li id="home"><Link to={'/'}>HOME</Link></li>
             <li id="trending"><Link to={'/trending'}>TRENDING</Link></li>
             <li id="tags"><Link to={'/tags'}>TAGS</Link></li>
         </ul>
-    </div>
+      </div>
     </nav>
   )
 }
